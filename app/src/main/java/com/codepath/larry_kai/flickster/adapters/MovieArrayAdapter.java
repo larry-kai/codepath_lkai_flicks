39d2c4b1de724841bf7714a2af64cb37db9094e8
Larry Kai <larry.kai@airbnb.com>
2016-08-05 21:24:14 -0700

    add landscape alternative

--- app/src/main/java/com/codepath/larry_kai/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/codepath/larry_kai/flickster/adapters/MovieArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.codepath.larry_kai.flickster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,7 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivImage;
+        ImageView ivBackDropImage;
     }
 
     public MovieArrayAdapter(Context context, List<Movie> movies) {
@@ -43,7 +45,6 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
             viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
             viewHolder.tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
-            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
             // Cache the viewHolder object inside the fresh view
             convertView.setTag(viewHolder);
         } else {
@@ -55,9 +56,22 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         viewHolder.tvTitle.setText(movie.getOriginalTitle());
         viewHolder.tvOverview.setText(movie.getOverview());
 
-        viewHolder.ivImage.setImageResource(0);
-        Picasso.with(getContext()).load(movie.getPosterPath()).
-                placeholder(R.drawable.placeholder).into(viewHolder.ivImage);
+
+
+        // Find the Image View
+        // Check orientation to determine which view to show :)
+        int orientation = getContext().getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivMovieBackDropImage);
+            viewHolder.ivImage.setImageResource(0); // clear out image from convertView
+            Picasso.with(getContext()).load(movie.getPosterPath()).
+                    placeholder(R.drawable.placeholder).into(viewHolder.ivImage);
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            viewHolder.ivBackDropImage = (ImageView) convertView.findViewById(R.id.ivMovieBackDropImage);
+            viewHolder.ivBackDropImage.setImageResource(0); // clear out image from convertView
+            Picasso.with(getContext()).load(movie.getBackdropPath()).
+                    placeholder(R.drawable.placeholder).into(viewHolder.ivBackDropImage);
+        }
 
         // return the view
         return convertView;
